90e517d7c516855e13095bef14a42a41e3fa85cc
Ellord Tayag <ellord@gmail.com>
2019-07-11 21:32:54 +1000

    Update next and remove next-typescript dep

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 // next.config.js
-const withTypescript = require('@zeit/next-typescript');
 
-module.exports = withTypescript({
+module.exports = {
   webpack(config, options) {
     return config;
   }
-});
+};
